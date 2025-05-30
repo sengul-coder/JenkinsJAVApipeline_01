640bf5671ccdbeb9a3e7868c5441cd95cb96dd24
sengul-coder <sengulturhan2015@gmail.com>
2025-05-30 10:58:49 +0100

    v5

--- src/C01.java
+++ src/C01.java
@@ -6,5 +6,6 @@ public class C01 {
         int randomSayi = (int) (Math.random()*80);
         System.out.println("randomSayi = " + randomSayi);
         System.out.println(randomSayi %2 == 0 ? "cift" : "tek");
+        System.out.println("update edildi");
 
 }}
---
+++ src/C04.java
@@ -0,0 +1,9 @@
+public class C04 {
+
+    public static void main(String[] args) {
+        System.out.println("yeni class c04");
+
+
+        System.out.println("son degisiklik");
+    }
+}
